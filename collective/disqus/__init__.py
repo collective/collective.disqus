9fe5e8a97dda5782318d4a8a986980a35676c4a3
hvelarde <hector.velarde@gmail.com>
2012-05-24 17:29:45 -0300

    added records to access the Disqus API to the control panel

--- collective/disqus/__init__.py
+++ collective/disqus/__init__.py
@@ -1,4 +1,7 @@
-  # -*- extra stuff goes here -*- 
+from zope.i18nmessageid import MessageFactory
+
+_ = MessageFactory('collective.disqus')
+
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
--- collective/disqus/interfaces.py
+++ collective/disqus/interfaces.py
@@ -1,14 +1,43 @@
-from collective.disqus import i18n
-from plone.app.discussion.interfaces import IDiscussionLayer
-from zope import interface
+from zope.interface import Interface
 from zope import schema
 
-class IDisqusLayer(IDiscussionLayer):
-    """Layer interface for collective.disqus add-on"""
+from plone.app.discussion.interfaces import IDiscussionLayer
 
-class IDisqusSettings(interface.Interface):
-    """Disqus service need settings"""
-    
-    activated = schema.Bool(title=i18n.activated)
-    
-    forum_short_name = schema.ASCIILine(title=i18n.forum_short_name)
+from collective.disqus import _
+from collective.disqus import i18n
+
+
+class IDisqusLayer(IDiscussionLayer):
+    """ Layer interface for collective.disqus add-on. """
+
+
+class IDisqusSettings(Interface):
+    """ Disqus service need settings. """
+
+    activated = schema.Bool(
+        title=i18n.activated,
+        description=_(u''),
+        )
+
+    forum_short_name = schema.ASCIILine(
+        title=i18n.forum_short_name,
+        description=_(u''),
+        )
+
+    access_token = schema.TextLine(
+        title=_(u'Access Token'),
+        description=_(u'Access token to retrive information from the Disqus forum.'),
+        required=True,
+        )
+
+    app_public_key = schema.TextLine(
+        title=_(u'Application public key'),
+        description=_(u'public key'),
+        required=True,
+        )
+
+    app_secret_key = schema.TextLine(
+        title=_(u'Application secret key'),
+        description=_(u'secret key'),
+        required=True,
+        )
