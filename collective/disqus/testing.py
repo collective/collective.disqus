21bb27568806aa8a4df799ab9cd0e7f0ffc58a75
hvelarde <hector.velarde@gmail.com>
2012-05-24 17:29:01 -0300

    added tests for setup and control panel

---
+++ collective/disqus/testing.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import FunctionalTesting
+
+
+class Fixture(PloneSandboxLayer):
+
+    defaultBases = (PLONE_FIXTURE,)
+
+    def setUpZope(self, app, configurationContext):
+        # Load ZCML
+        import collective.disqus
+        self.loadZCML(package=collective.disqus)
+
+    def setUpPloneSite(self, portal):
+        # Install into Plone site using portal_setup
+        self.applyProfile(portal, 'collective.disqus:default')
+
+
+FIXTURE = Fixture()
+INTEGRATION_TESTING = IntegrationTesting(
+    bases=(FIXTURE,),
+    name='collective.disqus:Integration',
+    )
+FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(FIXTURE,),
+    name='collective.disqus:Functional',
+    )
---
+++ collective/disqus/tests/test_setup.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+import unittest2 as unittest
+
+from plone.app.testing import TEST_USER_ID
+from plone.app.testing import setRoles
+
+from plone.browserlayer.utils import registered_layers
+
+from collective.disqus.config import PROJECTNAME
+from collective.disqus.testing import INTEGRATION_TESTING
+
+
+class InstallTest(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+
+    def test_installed(self):
+        qi = getattr(self.portal, 'portal_quickinstaller')
+        self.assertTrue(qi.isProductInstalled(PROJECTNAME))
+
+    def test_browserlayer_installed(self):
+        layers = [l.getName() for l in registered_layers()]
+        self.assertTrue('IDisqusLayer' in layers,
+                        'browser layer was not installed')
+
+
+class UninstallTest(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.qi = self.portal['portal_quickinstaller']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.qi.uninstallProducts(products=[PROJECTNAME])
+
+    def test_uninstalled(self):
+        self.assertFalse(self.qi.isProductInstalled(PROJECTNAME))
+
+    def test_browserlayer_removed(self):
+        layers = [l.getName() for l in registered_layers()]
+        self.assertFalse('IDisqusLayer' in layers,
+                         'browser layer was not removed')
