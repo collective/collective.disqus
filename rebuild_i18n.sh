64bda1f4e8376524791fb638f1eab27ebe611949
hvelarde <hector.velarde@gmail.com>
2012-12-07 13:32:31 -0200

    fix i18n rebuild script

--- rebuild_i18n.sh
+++ rebuild_i18n.sh
@@ -1,11 +1,11 @@
 #! /bin/sh
 
-PATH=bin:$PATH
 I18NDOMAIN="collective.disqus"
 BASE_DIRECTORY="collective/disqus"
 
 # Synchronise the templates and scripts with the .pot.
 i18ndude rebuild-pot --pot ${BASE_DIRECTORY}/locales/${I18NDOMAIN}.pot \
+    --merge ${BASE_DIRECTORY}/locales/manual.pot \
     --create ${I18NDOMAIN} \
     ${BASE_DIRECTORY}
 
@@ -14,5 +14,15 @@ for po in ${BASE_DIRECTORY}/locales/*/LC_MESSAGES/${I18NDOMAIN}.po; do
     i18ndude sync --pot ${BASE_DIRECTORY}/locales/${I18NDOMAIN}.pot $po
 done
 
+# Synchronise the templates and scripts with the .pot.
+i18ndude rebuild-pot --pot ${BASE_DIRECTORY}/locales/plone.pot \
+    --create plone \
+    ${BASE_DIRECTORY}/configure.zcml
+
+# Synchronise the Plone's pot file (Used for the workflows)
+for po in ${BASE_DIRECTORY}/locales/*/LC_MESSAGES/plone.po; do
+    i18ndude sync --pot ${BASE_DIRECTORY}/locales/plone.pot $po
+done
+
 # Report of errors and suspect untranslated messages
-i18ndude find-untranslated ${BASE_DIRECTORY}
+i18ndude find-untranslated -n ${BASE_DIRECTORY}
