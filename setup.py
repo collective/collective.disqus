f22fcf55daaa1558459c32a12a02681cd7410cad
hvelarde <hector.velarde@gmail.com>
2012-12-13 17:29:15 -0200

    Preparing release 2.0b1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '2.0dev'
+version = '2.0b1'
 long_description = open("README.txt").read() + "\n" + \
                    open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
                    open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
