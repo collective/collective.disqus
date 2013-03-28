df76fc21146a46ee5c649d3edac20cf4b97dfbdd
hvelarde <hector.velarde@gmail.com>
2013-03-28 18:41:17 -0300

    pep8

--- setup.py
+++ setup.py
@@ -3,32 +3,32 @@ import os
 
 version = '2.0b3.dev0'
 long_description = open("README.txt").read() + "\n" + \
-                   open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
-                   open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
-                   open(os.path.join("docs", "HISTORY.txt")).read()
+    open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
+    open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
+    open(os.path.join("docs", "HISTORY.txt")).read()
 
 setup(name='collective.disqus',
       version=version,
       description="Integration of Disqus comments platform API into Plone.",
       long_description=long_description,
       classifiers=[
-        "Development Status :: 4 - Beta",
-        "Environment :: Web Environment",
-        "Framework :: Plone",
-        "Framework :: Plone :: 4.1",
-        "Framework :: Plone :: 4.2",
-        "Framework :: Plone :: 4.3",
-        "Intended Audience :: End Users/Desktop",
-        "Intended Audience :: System Administrators",
-        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
-        "Operating System :: OS Independent",
-        "Programming Language :: JavaScript",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
-        "Topic :: Office/Business :: News/Diary",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
+          "Development Status :: 4 - Beta",
+          "Environment :: Web Environment",
+          "Framework :: Plone",
+          "Framework :: Plone :: 4.1",
+          "Framework :: Plone :: 4.2",
+          "Framework :: Plone :: 4.3",
+          "Intended Audience :: End Users/Desktop",
+          "Intended Audience :: System Administrators",
+          "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+          "Operating System :: OS Independent",
+          "Programming Language :: JavaScript",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: Office/Business :: News/Diary",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
       keywords='plone disqus comments api portlets',
       author='JeanMichel FRANCOIS aka toutpt',
       author_email='toutpt@gmail.com',
@@ -39,18 +39,18 @@ setup(name='collective.disqus',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-        'setuptools',
-        'disqus-python',
-        'Products.CMFPlone>=4.1',
-        'collective.prettydate',
-        ],
+          'collective.prettydate',
+          'disqus-python',
+          'Products.CMFPlone>=4.1',
+          'setuptools',
+      ],
       extras_require={
-        'test': [
-          'plone.app.testing',
-          'robotsuite',
-          'robotframework-selenium2library',
+          'test': [
+              'plone.app.testing',
+              'robotsuite',
+              'robotframework-selenium2library',
           ],
-        },
+      },
       entry_points="""
       [z3c.autoinclude.plugin]
       target = plone
